03b2a014ae98850b0ac32278a555c0fb2a35e8f4
Nadeem <nanvari@live.com>
2020-03-18 12:29:06 -0500

    reorganize code to call merge function within merge_sort function

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -40,33 +40,35 @@ C = [2, 7, 1, 5, 9, 22, 6]
 def merge_sort(arr):
     # TO-DO
     if len(arr) > 1:
-        left = arr[:(len(arr)//2)]
-        right = arr[(len(arr)//2):]
+        left = merge_sort(arr[:(len(arr)//2)])
+        right = merge_sort(arr[(len(arr)//2):])
 
-        merge_sort(left)
-        merge_sort(right)
+        # merge_sort(left)
+        # merge_sort(right)
 
-        i = 0
-        j = 0
-        k = 0
-        while i < len(left) and j < len(right):
-            if left[i] <= right[j]:
-                arr[k] = left[i]
-                i += 1
-                k += 1
-            else:
-                arr[k] = right[j]
-                j += 1
-                k += 1
-        while i < len(left):
-            arr[k] = left[i]
-            i += 1
-            k += 1
-        while j < len(right):
-            arr[k] = right[j]
-            j += 1
-            k += 1
-        return arr
+        arr = merge(left, right)
+
+        # i = 0
+        # j = 0
+        # k = 0
+        # while i < len(left) and j < len(right):
+        #     if left[i] <= right[j]:
+        #         arr[k] = left[i]
+        #         i += 1
+        #         k += 1
+        #     else:
+        #         arr[k] = right[j]
+        #         j += 1
+        #         k += 1
+        # while i < len(left):
+        #     arr[k] = left[i]
+        #     i += 1
+        #     k += 1
+        # while j < len(right):
+        #     arr[k] = right[j]
+        #     j += 1
+        #     k += 1
+        # return arr
 
     return arr
 
